6f27598614c8ffc2912ce9b75a7c53b417c3a753
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2022-12-01 23:10:03 +0000

    fixing issue

--- packages/react/src/lib/transformer.spec.ts
+++ packages/react/src/lib/transformer.spec.ts
@@ -6,13 +6,13 @@ describe('React Transformer', () => {
   // create a printer to convert the transformed source back to text
   const printer = ts.createPrinter();
 
-  describe('Props', () => {
-    it('should transform a prop into a useState hook', () => {
+  describe('State', () => {
+    it('should transform state into a useState hook', () => {
       const source = `
-      import { Component } from '@emblazon/core';
+      import { Component, State } from '@emblazon/core';
       @Component()
       export class Test {
-        @Prop() readonly test: string;
+        @State() test: string;
 
         render() {
           return <div />;
@@ -21,19 +21,19 @@ describe('React Transformer', () => {
     `;
       const components = transform(source, transformer);
       const result = components[0];
-      const props = result.props as ts.VariableStatement[];
+      const props = result.states as ts.VariableStatement[];
 
       expect(printNode(props[0])).toMatchInlineSnapshot(
         '"const [test, setTest] = useState<string>();"'
       );
     });
 
-    it('should transform a prop into a useState hook with a default value', () => {
+    it('should transform state into a useState hook with a default value', () => {
       const source = `
-      import { Component } from '@emblazon/core';
+      import { Component, State } from '@emblazon/core';
       @Component()
       export class Test {
-        @Prop() readonly test: string = 'test';
+        @State() test: string = 'test';
 
         render() {
           return <div />;
@@ -42,19 +42,19 @@ describe('React Transformer', () => {
     `;
       const components = transform(source, transformer);
       const result = components[0];
-      const props = result.props as ts.VariableStatement[];
+      const props = result.states as ts.VariableStatement[];
 
       expect(printNode(props[0])).toMatchInlineSnapshot(
         '"const [test, setTest] = useState<string>(\\"test\\");"'
       );
     });
 
-    it('should transform a prop into a useState hook with a default value without a type', () => {
+    it('should transform state into a useState hook with a default value without a type', () => {
       const source = `
-      import { Component } from '@emblazon/core';
+      import { Component, State } from '@emblazon/core';
       @Component()
       export class Test {
-        @Prop() readonly test = 'test';
+        @State() test = 'test';
 
         render() {
           return <div />;
@@ -63,19 +63,19 @@ describe('React Transformer', () => {
     `;
       const components = transform(source, transformer);
       const result = components[0];
-      const props = result.props as ts.VariableStatement[];
+      const props = result.states as ts.VariableStatement[];
 
       expect(printNode(props[0])).toMatchInlineSnapshot(
         '"const [test, setTest] = useState(\\"test\\");"'
       );
     });
 
-    it('should transform a prop into a useState hook with a default value from a function', () => {
+    it('should transform state into a useState hook with a default value from a function', () => {
       const source = `
-      import { Component } from '@emblazon/core';
+      import { Component, State } from '@emblazon/core';
       @Component()
       export class Test {
-        @Prop() readonly test: string = () => 'test';
+        @State() test: string = () => 'test';
 
         render() {
           return <div />;
@@ -84,19 +84,19 @@ describe('React Transformer', () => {
     `;
       const components = transform(source, transformer);
       const result = components[0];
-      const props = result.props as ts.VariableStatement[];
+      const props = result.states as ts.VariableStatement[];
 
       expect(printNode(props[0])).toMatchInlineSnapshot(
         '"const [test, setTest] = useState<string>(() => \\"test\\");"'
       );
     });
 
-    it('should transform a prop into a useState hook with a default number value', () => {
+    it('should transform state into a useState hook with a default number value', () => {
       const source = `
-      import { Component } from '@emblazon/core';
+      import { Component, State } from '@emblazon/core';
       @Component()
       export class Test {
-        @Prop() readonly test: number = 1;
+        @State() test: number = 1;
 
         render() {
           return <div />;
@@ -105,19 +105,19 @@ describe('React Transformer', () => {
     `;
       const components = transform(source, transformer);
       const result = components[0];
-      const props = result.props as ts.VariableStatement[];
+      const props = result.states as ts.VariableStatement[];
 
       expect(printNode(props[0])).toMatchInlineSnapshot(
         '"const [test, setTest] = useState<number>(1);"'
       );
     });
 
-    it('should transform a prop into a useState hook with a default boolean value', () => {
+    it('should transform state into a useState hook with a default boolean value', () => {
       const source = `
-      import { Component } from '@emblazon/core';
+      import { Component, State } from '@emblazon/core';
       @Component()
       export class Test {
-        @Prop() readonly test: boolean = true;
+        @State() test: boolean = true;
 
         render() {
           return <div />;
@@ -126,19 +126,19 @@ describe('React Transformer', () => {
     `;
       const components = transform(source, transformer);
       const result = components[0];
-      const props = result.props as ts.VariableStatement[];
+      const props = result.states as ts.VariableStatement[];
 
       expect(printNode(props[0])).toMatchInlineSnapshot(
         '"const [test, setTest] = useState<boolean>(true);"'
       );
     });
 
-    it('should transform a prop into a useState hook with a default array value', () => {
+    it('should transform state into a useState hook with a default array value', () => {
       const source = `
-      import { Component } from '@emblazon/core';
+      import { Component, State } from '@emblazon/core';
       @Component()
       export class Test {
-        @Prop() readonly test: string[] = ['test'];
+        @State() test: string[] = ['test'];
 
         render() {
           return <div />;
@@ -147,11 +147,34 @@ describe('React Transformer', () => {
     `;
       const components = transform(source, transformer);
       const result = components[0];
-      const props = result.props as ts.VariableStatement[];
+      const props = result.states as ts.VariableStatement[];
 
       expect(printNode(props[0])).toMatchInlineSnapshot(
         '"const [test, setTest] = useState<string[]>([\\"test\\"]);"'
       );
+    });
+
+    it('should transform state into a useState hook with a default object value', () => {
+      const source = `
+      import { Component, State } from '@emblazon/core';
+      @Component()
+      export class Test {
+        @State() test: { test: string } = { test: 'test' };
+
+        render() {
+          return <div />;
+        }
+      }
+    `;
+      const components = transform(source, transformer);
+      const result = components[0];
+      const props = result.states as ts.VariableStatement[];
+
+      expect(printNode(props[0])).toMatchInlineSnapshot(`
+        "const [test, setTest] = useState<{
+            test: string;
+        }>({ test: \\"test\\" });"
+      `);
     });
   });
 
--- packages/react/src/lib/transformer.ts
+++ packages/react/src/lib/transformer.ts
@@ -3,13 +3,13 @@ import * as ts from 'typescript';
 import { factory } from 'typescript';
 
 export interface ReactTransformer extends Transformer {
-  Prop?: (value: ts.PropertyDeclaration) => ts.VariableStatement;
+  State?: (value: ts.PropertyDeclaration) => ts.VariableStatement;
 }
 
 export const transformer: ReactTransformer = {
-  Prop: (prop) => {
-    // get the name of the prop
-    const getterName = prop.name.getText();
+  State: (state) => {
+    // get the name of the state
+    const getterName = state.name.getText();
 
     // create a new name for the prop setter
     const setterName = `set${getterName[0].toUpperCase()}${getterName.slice(
@@ -17,10 +17,10 @@ export const transformer: ReactTransformer = {
     )}`;
 
     // get the type of the prop if it exists
-    const type = prop.type;
+    const type = state.type;
 
     // get the initializer of the prop if it exists
-    const initializer = prop.initializer;
+    const initializer = state.initializer;
 
     // convert the property to a useState hook
     return factory.createVariableStatement(
