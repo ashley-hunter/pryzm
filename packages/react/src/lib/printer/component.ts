8db64cee53f1dbc0f5578a14f9d71753f8bee1b0
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2023-02-28 16:54:19 +0000

    adding ref support

--- packages/react/src/lib/printer/component.ts
+++ packages/react/src/lib/printer/component.ts
@@ -2,67 +2,140 @@ import { TransformerResult } from '@pryzm/compiler';
 import * as ts from 'typescript';
 import { factory } from 'typescript';
 import { ReactTransformer } from '../transformer';
-import { propsName } from '../utils/names';
 
 export function createComponent(metadata: TransformerResult<ReactTransformer>) {
-  return factory.createVariableStatement(
+  return factory.createFunctionDeclaration(
     [factory.createToken(ts.SyntaxKind.ExportKeyword)],
-    factory.createVariableDeclarationList(
+    undefined,
+    factory.createIdentifier('UxaAlert'),
+    undefined,
+    [
+      factory.createParameterDeclaration(
+        undefined,
+        undefined,
+        factory.createObjectBindingPattern(metadata.props.map(prop => prop.destructuredProperty)),
+        undefined,
+        undefined,
+        undefined
+      ),
+    ],
+    undefined,
+    factory.createBlock(
       [
-        factory.createVariableDeclaration(
-          factory.createIdentifier(metadata.name),
-          undefined,
-          undefined,
-          factory.createCallExpression(
-            factory.createIdentifier('forwardRef'),
-            [
-              factory.createTypeReferenceNode(factory.createIdentifier('HTMLElement'), undefined),
-              factory.createTypeReferenceNode(
-                factory.createIdentifier(propsName(metadata.name)),
-                undefined
-              ),
-            ],
-            [
-              factory.createArrowFunction(
-                undefined,
-                undefined,
-                [
-                  factory.createParameterDeclaration(
-                    undefined,
-                    undefined,
-                    factory.createObjectBindingPattern(
-                      metadata.props.map(prop => prop.destructuredProperty)
-                    ),
-                    undefined,
-                    undefined,
-                    undefined
-                  ),
-                  factory.createParameterDeclaration(
-                    undefined,
-                    undefined,
-                    factory.createIdentifier('ref'),
-                    undefined,
-                    undefined,
-                    undefined
-                  ),
-                ],
-                undefined,
-                factory.createToken(ts.SyntaxKind.EqualsGreaterThanToken),
-                factory.createBlock(
-                  [
-                    ...metadata.states.map(state => state.statement),
-                    ...metadata.computed.map(computed => computed.statement),
-                    ...metadata.methods.map(method => method.statement),
-                    factory.createReturnStatement(metadata.template),
-                  ],
-                  true
-                )
-              ),
-            ]
-          )
-        ),
+        ...metadata.refs.map(ref => ref.statement),
+        ...metadata.states.map(state => state.statement),
+        ...metadata.computed.map(computed => computed.statement),
+        ...metadata.methods.map(method => method.statement),
+        factory.createReturnStatement(metadata.template),
       ],
-      ts.NodeFlags.Const
+      true
     )
   );
+
+  // return factory.createVariableStatement(
+  //   [factory.createToken(ts.SyntaxKind.ExportKeyword)],
+  //   factory.createVariableDeclarationList(
+  //     [
+  //       factory.createVariableDeclaration(
+  //         factory.createIdentifier(metadata.name),
+  //         undefined,
+  //         factory.createTypeReferenceNode(factory.createIdentifier('FC'), [
+  //           factory.createTypeReferenceNode(
+  //             factory.createIdentifier(propsName(metadata.name)),
+  //             undefined
+  //           ),
+  //         ]),
+  //         factory.createArrowFunction(
+  //           undefined,
+  //           undefined,
+  //           [
+  //             factory.createParameterDeclaration(
+  //               undefined,
+  //               undefined,
+  //               factory.createObjectBindingPattern(
+  //                 metadata.props.map(prop => prop.destructuredProperty)
+  //               ),
+  //               undefined,
+  //               undefined,
+  //               undefined
+  //             ),
+  //           ],
+  //           undefined,
+  //           factory.createToken(ts.SyntaxKind.EqualsGreaterThanToken),
+  //           factory.createBlock(
+  //             [
+  //               ...metadata.states.map(state => state.statement),
+  //               ...metadata.computed.map(computed => computed.statement),
+  //               ...metadata.methods.map(method => method.statement),
+  //               factory.createReturnStatement(metadata.template),
+  //             ],
+  //             true
+  //           )
+  //         )
+  //       ),
+  //     ],
+  //     ts.NodeFlags.Const
+  //   )
+  // );
+
+  // return factory.createVariableStatement(
+  //   [factory.createToken(ts.SyntaxKind.ExportKeyword)],
+  //   factory.createVariableDeclarationList(
+  //     [
+  //       factory.createVariableDeclaration(
+  //         factory.createIdentifier(metadata.name),
+  //         undefined,
+  //         undefined,
+  //         factory.createCallExpression(
+  //           factory.createIdentifier('forwardRef'),
+  //           [
+  //             factory.createTypeReferenceNode(factory.createIdentifier('HTMLElement'), undefined),
+  //             factory.createTypeReferenceNode(
+  //               factory.createIdentifier(propsName(metadata.name)),
+  //               undefined
+  //             ),
+  //           ],
+  //           [
+  //             factory.createArrowFunction(
+  //               undefined,
+  //               undefined,
+  //               [
+  //                 factory.createParameterDeclaration(
+  //                   undefined,
+  //                   undefined,
+  //                   factory.createObjectBindingPattern(
+  //                     metadata.props.map(prop => prop.destructuredProperty)
+  //                   ),
+  //                   undefined,
+  //                   undefined,
+  //                   undefined
+  //                 ),
+  //                 factory.createParameterDeclaration(
+  //                   undefined,
+  //                   undefined,
+  //                   factory.createIdentifier('ref'),
+  //                   undefined,
+  //                   undefined,
+  //                   undefined
+  //                 ),
+  //               ],
+  //               undefined,
+  //               factory.createToken(ts.SyntaxKind.EqualsGreaterThanToken),
+  //               factory.createBlock(
+  //                 [
+  //                   ...metadata.states.map(state => state.statement),
+  //                   ...metadata.computed.map(computed => computed.statement),
+  //                   ...metadata.methods.map(method => method.statement),
+  //                   factory.createReturnStatement(metadata.template),
+  //                 ],
+  //                 true
+  //               )
+  //             ),
+  //           ]
+  //         )
+  //       ),
+  //     ],
+  //     ts.NodeFlags.Const
+  //   )
+  // );
 }
