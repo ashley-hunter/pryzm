7e6aa601964c1f31e69aa60fe46c80f03a248b50
Ashley Hunter <ashley-glenn.hunter@microfocus.com>
2023-03-10 10:43:13 +0000

    shrinking logo

--- apps/playground/src/app/app.tsx
+++ apps/playground/src/app/app.tsx
@@ -123,8 +123,8 @@ export class App {
         <div className="mx-auto px-2 sm:px-6 lg:px-8">
           <div className="relative flex h-12 items-center justify-between">
             <div className="flex flex-1 items-center justify-center sm:justify-start">
-              <img className="h-4" src="/logo.svg" alt="Logo" />
-              <small className="text-xs font-medium text-gray-400 pl-1 mt-2">v0.1</small>
+              <img className="h-3.5" src="/logo.svg" alt="Logo" />
+              <small className="text-xs font-medium text-gray-400 pl-1 mt-1.5">v0.1</small>
 
               <select
                 className="ml-auto inline-flex justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100"
